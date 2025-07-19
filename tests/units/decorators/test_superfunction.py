8861ab0248680111ad44c739b320dbf5c3cd2cfc
esblinov <esblinov@avito.ru>
2025-07-19 15:49:02 +0300

    more tests

--- tests/units/decorators/test_superfunction.py
+++ tests/units/decorators/test_superfunction.py
@@ -2,17 +2,17 @@ import io
 from asyncio import run
 from contextlib import redirect_stdout
 
-from transfunctions import superfunction, sync_context, async_context
+from transfunctions import superfunction, sync_context, async_context, generator_context
 
 """
 Что нужно проверить:
 
-1. Все базово работает без аргументов и с аргументами, для обычных, асинк и генераторных функций.
+
 2. При попытке навесить декоратор @superfunction на функцию с return'ами будет исключение.
 
 Что проверено:
 
--
+1. Все базово работает без аргументов и с аргументами, для обычных, асинк и генераторных функций.
 """
 
 
@@ -23,6 +23,8 @@ def test_just_sync_call():
             print(1)
         with async_context:
             print(2)
+        with generator_context:
+            yield from [1, 2, 3]
 
     buffer = io.StringIO()
     with redirect_stdout(buffer):
@@ -37,8 +39,78 @@ def test_just_async_call():
             print(1)
         with async_context:
             print(2)
+        with generator_context:
+            yield from [1, 2, 3]
 
     buffer = io.StringIO()
     with redirect_stdout(buffer):
         run(function())
     assert buffer.getvalue() == "2\n"
+
+
+def test_just_generator_iteration():
+    @superfunction
+    def function():
+        with sync_context:
+            print(1)
+        with async_context:
+            print(2)
+        with generator_context:
+            yield from [1, 2, 3]
+
+    buffer = io.StringIO()
+    with redirect_stdout(buffer):
+        numbers = list(function())
+
+    assert buffer.getvalue() == ""
+    assert numbers == [1, 2, 3]
+
+
+def test_just_sync_call_with_arguments():
+    @superfunction
+    def function(a, b):
+        with sync_context:
+            print(a)
+        with async_context:
+            print(b)
+        with generator_context:
+            yield from [1, 2, 3]
+
+    buffer = io.StringIO()
+    with redirect_stdout(buffer):
+        function(1, 2)
+    assert buffer.getvalue() == "1\n"
+
+
+def test_just_async_call_with_arguments():
+    @superfunction
+    def function(a, b):
+        with sync_context:
+            print(a)
+        with async_context:
+            print(b)
+        with generator_context:
+            yield from [1, 2, 3]
+
+    buffer = io.StringIO()
+    with redirect_stdout(buffer):
+        run(function(1, 2))
+    assert buffer.getvalue() == "2\n"
+
+
+def test_just_generator_with_arguments_iteration():
+    @superfunction
+    def function(a, b):
+        with sync_context:
+            print(a)
+        with async_context:
+            print(b)
+        with generator_context:
+            yield from [a, b, 3]
+
+    buffer = io.StringIO()
+    with redirect_stdout(buffer):
+        numbers = list(function(1, 2))
+
+    assert buffer.getvalue() == ""
+    assert numbers == [1, 2, 3]
