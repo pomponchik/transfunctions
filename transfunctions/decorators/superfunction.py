4e196d0a8a448e5a3e98c8ea8e60a62ee75e7859
esblinov <esblinov@avito.ru>
2025-07-22 20:14:25 +0300

    now you can tilde syntax or simple call syntax

--- transfunctions/decorators/superfunction.py
+++ transfunctions/decorators/superfunction.py
@@ -3,7 +3,7 @@ import weakref
 from ast import NodeTransformer, Return, AST
 from inspect import currentframe
 from functools import wraps
-from typing import Dict, Any, Optional, Union, List
+from typing import Dict, Any, Optional, Union, List, Callable
 from collections.abc import Coroutine
 
 if sys.version_info <= (3, 10):  # pragma: no cover
@@ -23,13 +23,14 @@ else:  # pragma: no cover
     CoroutineClass: TypeAlias = Coroutine[Any, Any, None]
 
 class UsageTracer(CoroutineClass):
-    def __init__(self, args, kwargs, transformer) -> None:
+    def __init__(self, args, kwargs, transformer, tilde_syntax: bool) -> None:
         self.flags: Dict[str, bool] = {}
         self.args = args
         self.kwargs = kwargs
         self.transformer = transformer
+        self.tilde_syntax = tilde_syntax
         self.coroutine = self.async_option(self.flags, args, kwargs, transformer)
-        self.finalizer = weakref.finalize(self, self.sync_option, self.flags, args, kwargs, transformer, self.coroutine)
+        self.finalizer = weakref.finalize(self, self.sync_option, self.flags, args, kwargs, transformer, self.coroutine, tilde_syntax)
 
     def __iter__(self):
         self.flags['used'] = True
@@ -42,6 +43,9 @@ class UsageTracer(CoroutineClass):
         return self.coroutine.__await__()
 
     def __invert__(self):
+        if not self.tilde_syntax:
+            raise NotImplementedError('The syntax with ~ is disabled for this superfunction. Call it with simple breackets.')
+
         result = self.finalizer()
         return result
 
@@ -55,10 +59,13 @@ class UsageTracer(CoroutineClass):
         pass
 
     @staticmethod
-    def sync_option(flags: Dict[str, bool], args, kwargs, transformer, wrapped_coroutine: CoroutineClass) -> None:
+    def sync_option(flags: Dict[str, bool], args, kwargs, transformer, wrapped_coroutine: CoroutineClass, tilde_syntax: bool) -> None:
         if not flags.get('used', False):
             wrapped_coroutine.close()
-            return transformer.get_usual_function()(*args, **kwargs)
+            if not tilde_syntax:
+                return transformer.get_usual_function()(*args, **kwargs)
+            else:
+                raise NotImplementedError(f'The tilde-syntax is enabled for the "{transformer.function.__name__}" function. Call it like this: ~{transformer.function.__name__}().')
 
     @staticmethod
     async def async_option(flags: Dict[str, bool], args, kwargs, transformer) -> None:
@@ -68,24 +75,29 @@ class UsageTracer(CoroutineClass):
 
 not_display(UsageTracer)
 
-def superfunction(function):
-    class NoReturns(NodeTransformer):
-        def visit_Return(self, node: Return) -> Optional[Union[AST, List[AST]]]:
-            raise WrongTransfunctionSyntaxError('A superfunction cannot contain a return statement.')
+def superfunction(*args: Callable, tilde_syntax: bool = True):
+    def decorator(function):
+        class NoReturns(NodeTransformer):
+            def visit_Return(self, node: Return) -> Optional[Union[AST, List[AST]]]:
+                raise WrongTransfunctionSyntaxError('A superfunction cannot contain a return statement.')
 
-    transformer = FunctionTransformer(
-        function,
-        currentframe().f_back.f_lineno,
-        'superfunction',
-        extra_transformers=[
-            #NoReturns(),
-        ],
-    )
+        transformer = FunctionTransformer(
+            function,
+            currentframe().f_back.f_lineno,
+            'superfunction',
+            extra_transformers=[
+                #NoReturns(),
+            ],
+        )
 
-    @wraps(function)
-    def wrapper(*args, **kwargs):
-        return UsageTracer(args, kwargs, transformer)
+        @wraps(function)
+        def wrapper(*args, **kwargs):
+            return UsageTracer(args, kwargs, transformer, tilde_syntax)
 
-    wrapper.__is_superfunction__ = True
+        wrapper.__is_superfunction__ = True
 
-    return wrapper
+        return wrapper
+
+    if args:
+        return decorator(args[0])
+    return decorator
