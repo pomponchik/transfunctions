5c0552250115b6a602d2156515b7d00464c7627a
esblinov <esblinov@avito.ru>
2025-07-30 20:24:03 +0300

    delete some changes about frames

--- transfunctions/decorators/transfunction.py
+++ transfunctions/decorators/transfunction.py
@@ -8,12 +8,4 @@ from transfunctions._typing import Callable, FunctionParams, ReturnType
 def transfunction(
     function: Callable[FunctionParams, ReturnType],
 ) -> FunctionTransformer[FunctionParams, ReturnType]:
-
-    current_frame = currentframe()
-
-    if current_frame is None or current_frame.f_back is None:
-        raise AmbiguousFrameSyntaxError(
-            "No stack frame found. This is likely due to calling this code from dynamically evaluated contexts."
-        )
-
-    return FunctionTransformer(function, current_frame.f_back.f_lineno, "transfunction")
+    return FunctionTransformer(function, currentframe().f_back.f_lineno, "transfunction")
