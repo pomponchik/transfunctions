7715c63256fde39185970e9f81b27883d0a1e0b9
Евгений Блинов <pomponchik@Mac-mini-Evgenij.local>
2026-02-10 18:15:28 +0300

    Refactor transfunction decorator to support variadic args

--- transfunctions/decorators/transfunction.py
+++ transfunctions/decorators/transfunction.py
@@ -1,18 +1,28 @@
 from inspect import currentframe
 from types import FrameType
-from typing import cast
+from typing import Union, cast
 
 from transfunctions.transformer import FunctionTransformer
 from transfunctions.typing import Callable, FunctionParams, ReturnType
 
 
 def transfunction(
-    function: Callable[FunctionParams, ReturnType],
-) -> FunctionTransformer[FunctionParams, ReturnType]:
-    return FunctionTransformer(
-        function,
-        cast(FrameType, cast(FrameType, currentframe()).f_back).f_lineno,
-        "transfunction",
-        cast(FrameType, cast(FrameType, currentframe()).f_back),
-        True,
-    )
+    *args: Callable[FunctionParams, ReturnType], check_decorators: bool = True,
+) -> Union[Callable[[Callable[FunctionParams, ReturnType]], FunctionTransformer[FunctionParams, ReturnType]], FunctionTransformer[FunctionParams, ReturnType]]:
+    frame = currentframe()
+
+    def decorator(
+        function: Callable[FunctionParams, ReturnType],
+    ) -> FunctionTransformer[FunctionParams, ReturnType]:
+        return FunctionTransformer(
+            function,
+            cast(FrameType, cast(FrameType, frame).f_back).f_lineno,
+            "transfunction",
+            cast(FrameType, cast(FrameType, frame).f_back),
+            check_decorators,
+        )
+
+    if args:
+        return decorator(args[0])
+
+    return decorator
